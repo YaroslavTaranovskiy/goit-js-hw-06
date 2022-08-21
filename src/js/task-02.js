ed8598662d65ae49723b18d7d9f068b12d912dcc
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2022-08-21 18:40:36 +0300

    task-2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulElement = document.querySelector('ul');
+const elements = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const ingredient = ingredients[i];
+  const liElement = document.createElement('li');
+  liElement.textContent = ingredient;
+  liElement.classList.add('item');
+
+  elements.push(liElement);
+}
+
+ulElement.append(...elements);
