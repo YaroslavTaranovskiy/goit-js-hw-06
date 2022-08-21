2b51cf97d054619501adbcbf3b4d616a4ede5137
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2022-08-21 22:21:23 +0300

    task-8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const checkForm = document.querySelector(".login-form");
 
+checkForm.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  
+  const formData = new FormData(event.currentTarget);
+  const data = {};
+  formData.forEach((name, value) => {
+    if (!name || !value) {
+      alert("Все поля должны быть заполнены...");
+    } else {
+      data[value] = name;
+    }
+  });
+  if (data.email && data.password) {
+    console.log(data);
+    event.currentTarget.reset();
+  }
+}
