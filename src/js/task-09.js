d863576a8763c14b4b7bea77c8832eb989502cfe
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2022-08-21 22:25:54 +0300

    task-9 done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body;
+const span = document.querySelector(".color");
+const chgBtn = document.querySelector(".change-color");
+
+chgBtn.addEventListener("click", changeBgndColor);
+
+function changeBgndColor() {
+  const ourColor = getRandomHexColor();
+  body.style.backgroundColor = `${ourColor}`;
+  span.textContent = `${ourColor}`;
+}
