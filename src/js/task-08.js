8b4fd086f7fa919c8151e8fee3ae60777fdfc1b5
YaroslavTaranovskiy <103592817+YaroslavTaranovskiy@users.noreply.github.com>
2022-09-05 15:37:59 +0300

    task -8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -4,17 +4,27 @@ checkForm.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   
-  const formData = new FormData(event.currentTarget);
-  const data = {};
-  formData.forEach((name, value) => {
-    if (!name || !value) {
-      alert("Все поля должны быть заполнены...");
-    } else {
-      data[value] = name;
-    }
-  });
-  if (data.email && data.password) {
-    console.log(data);
-    event.currentTarget.reset();
+  // const formData = new FormData(event.currentTarget);
+  // const data = {};
+  // formData.forEach((name, value) => {
+  //   if (!name || !value) {
+  //     alert("Все поля должны быть заполнены...");
+  //   } else {
+  //     data[value] = name;
+  //   }
+  // });
+  // if (data.email && data.password) {
+  //   console.log(data);
+  //   event.currentTarget.reset();
+  // }
+
+  const formEl = event.currentTarget.elements;
+  const email = formEl.email;
+  const password = formEl.password;
+
+  if (email.value === "" || password.value === "") {
+    return alert('Все поля должны быть заполнены...');
   }
+  console.log({ email: email.value, password: password.value });
+  checkForm.reset();
 }
